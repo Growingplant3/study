4f4312438857a4313f8ee6f4c028677f1123ffc4
下田　佳紀 <y.s@experience.local>
2020-02-18 23:33:31 +0900

    ユーザーテーブルとそのカラムの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_143336) do
+ActiveRecord::Schema.define(version: 2020_02_18_140451) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sex", default: true
+    t.integer "age"
+    t.string "password"
   end
 
 end
