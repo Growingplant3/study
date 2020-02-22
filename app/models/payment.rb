997601e8b56b8940665592692783cc719b7a0f13
下田　佳紀 <y.s@experience.local>
2020-02-23 01:50:41 +0900

    enumの導入〜修正２

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,17 @@
 class Payment < ApplicationRecord
-    enum category:{food:0,daily:1,transport:2,entertainment:3,education:4,insurance:5,medical:6,communication:7,utilities:8,rent:9,saving:10,other:11,salary:12}
+    enum category: {
+        food: 0,
+        daily: 1,
+        transport: 2,
+        entertainment: 3,
+        education: 4,
+        insurance: 5,
+        medical: 6,
+        communication: 7,
+        utilities: 8,
+        rent: 9,
+        saving: 10,
+        other: 11,
+        salary: 12,
+    }
 end
