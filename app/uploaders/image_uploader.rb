80e57c9d31648a07013fd0845036b2f10c694325
下田　佳紀 <y.s@experience.local>
2020-03-03 01:31:16 +0900

    色々修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -73,4 +73,5 @@ class ImageUploader < CarrierWave::Uploader::Base
       name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
       name.downcase
     end
+  end
 end
