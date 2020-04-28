40f8007240c598e547deb2ce84a06ecd740a0501
下田　佳紀 <y.s@experience.local>
2020-04-28 23:06:23 +0900

    bootstrap3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,12 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    #@users = User.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = current_user
   end
 
   # GET /users/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  root to: "users#index"
+  resources :users, only: [:index, :show, :edit]
   resources :payments
-  root 'users#index'
+  devise_for :users
+  #,controllers: {
+  #  registrations: 'users/registrations'
+  #  sessions: 'users/sessions'
+  #}
+  #devise_scope :user do
+  #  get 'users/index', to: 'users/registrations#index'
+  #  get 'users/destroy', to: 'users/sessions#destroy'
+  #end
 end
