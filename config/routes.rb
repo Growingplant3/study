8fdfbe4a726fade418a6af99e27016cfa6e9cdee
下田　佳紀 <y.s@experience.local>
2020-04-29 03:41:34 +0900

    bootstrap6

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  resources :users, only: [:index, :show, :edit]
-  resources :payments
   devise_for :users
-  #,controllers: {
-  #  registrations: 'users/registrations'
-  #  sessions: 'users/sessions'
-  #}
-  #devise_scope :user do
-  #  get 'users/index', to: 'users/registrations#index'
-  #  get 'users/destroy', to: 'users/sessions#destroy'
-  #end
+  resources :users, only: [:show]
+  resources :payments
 end
