37ff81a69ec5e1e93fdf4f15a1c337ae53888180
下田　佳紀 <y.s@experience.local>
2020-04-30 02:20:22 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#show"
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  get "users/show" => "users/show"
+  get "users/show" => "users#show"
   resources :users, only: [:show]
   resources :payments
 end
